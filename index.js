4bd782409fb06e5845a472d8d9366eab31d56143
caicindy87 <caicindy87@gmail.com>
2020-04-20 10:28:33 -0700

    Done.

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ let dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = "0px";
 
-function moveDodgerLeft() {
+function setLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
+  return left;
+}
 
+function moveDodgerLeft() {
+  let left = setLeft();
   if (left > 0) {
     dodger.style.left = `${left - 1}px`;
   }
 }
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
-
+  let left = setLeft();
   if (left < 360) {
     dodger.style.left = `${left + 1}px`;
   }
